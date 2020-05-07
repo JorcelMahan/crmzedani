0478d82b16930fd2dba3edc59a3406021470ecff
joma <johanmfc11@gmail.com>
2020-05-07 17:27:29 -0400

    a little change

--- config/apollo.js
+++ config/apollo.js
@@ -2,10 +2,10 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
-const uridevelop = 'http://localhost:4000/';
+// const uridevelop = 'http://localhost:4000/';
 const uriproduction = 'https://warm-island-75318.herokuapp.com/';
 const HttpLink = createHttpLink({
-  uri: uridevelop,
+  uri: uriproduction,
   fetch,
 });
 
