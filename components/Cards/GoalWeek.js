2c1e71347acb09bb5c5e02fad4395e64382f2c64
joma <johanmfc11@gmail.com>
2020-09-29 08:46:52 -0400

    add felicitaciones goal

--- components/Cards/GoalWeek.js
+++ components/Cards/GoalWeek.js
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { gql, useQuery } from '@apollo/client';
 import Loader from '../Loader';
+import { CardActionArea, CardMedia } from '@material-ui/core';
 
 const GET_TOTAL_SALES_CURRENT_MONTH = gql`
   query totalSalesCurrentMonth {
@@ -54,6 +55,17 @@ const useStyles = makeStyles({
   },
   pos: {
     marginBottom: '1rem',
+  },
+  containerMeta: {
+    maxWidth: 345,
+  },
+  mediaMeta: {
+    height: 140,
+  },
+  divContainer: {
+    marginTop: '1rem',
+    display: 'flex',
+    justifyContent: 'center',
   },
 });
 
@@ -124,76 +136,100 @@ export default function GoalWeek() {
   if (loading) return <Loader />;
   if (error) return <p>Error {error.message}</p>;
   return (
-    <Grid container spacing={2} className={classes.root}>
-      <Grid item xs={12} sm={3}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color='textSecondary'
-              gutterBottom
-            >
-              {/* Hoy: {new Date().toDateString()} */}
-              Miraflores
-            </Typography>
-            <Typography variant='h5' component='h2'>
-              <CountDayStore store='miraflores' />
-            </Typography>
-          </CardContent>
-        </Card>
+    <>
+      <Grid container spacing={2} className={classes.root}>
+        <Grid item xs={12} sm={3}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color='textSecondary'
+                gutterBottom
+              >
+                {/* Hoy: {new Date().toDateString()} */}
+                Miraflores
+              </Typography>
+              <Typography variant='h5' component='h2'>
+                <CountDayStore store='miraflores' />
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color='textSecondary'
+                gutterBottom
+              >
+                San Miguel
+              </Typography>
+              <Typography variant='h5' component='h2'>
+                <CountDayStore store='san miguel' />
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color='textSecondary'
+                gutterBottom
+              >
+                Sopocachi
+              </Typography>
+              <Typography variant='h5' component='h2'>
+                <CountDayStore store='sopocachi' />
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color='textSecondary'
+                gutterBottom
+              >
+                Mes:
+                {new Date().toLocaleString('es-MX', { month: 'long' })}
+              </Typography>
+              <Typography variant='h5' component='h2'>
+                <Count month='8' startDay='1' endDay='30' /> de <b> 1200 </b>
+                <br />
+                <hr />
+                Falta: {1200 - data.totalSalesCurrentMonth}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={3}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color='textSecondary'
-              gutterBottom
-            >
-              San Miguel
-            </Typography>
-            <Typography variant='h5' component='h2'>
-              <CountDayStore store='san miguel' />
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={3}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color='textSecondary'
-              gutterBottom
-            >
-              Sopocachi
-            </Typography>
-            <Typography variant='h5' component='h2'>
-              <CountDayStore store='sopocachi' />
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={3}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color='textSecondary'
-              gutterBottom
-            >
-              Mes:
-              {new Date().toLocaleString('es-MX', { month: 'long' })}
-            </Typography>
-            <Typography variant='h5' component='h2'>
-              <Count month='8' startDay='1' endDay='30' /> de <b> 1200 </b>
-              <br />
-              <hr />
-              Falta: {1200 - data.totalSalesCurrentMonth}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+      {data.totalSalesCurrentMonth >= 1200 && (
+        <div className={classes.divContainer}>
+          <Card className={classes.containerMeta}>
+            <CardActionArea>
+              <CardMedia
+                image='https://t3.ftcdn.net/jpg/02/44/50/06/240_F_244500619_Nh1WCn4rggy7vAxmqwyEY9zL5s2PMh2Z.jpg'
+                className={classes.mediaMeta}
+                title='Congratulations'
+              />
+            </CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant='h1' component='h2'>
+                Lo logramos
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                ¡Eso es! Lo hicimos. Tomemonos unos minutos para celebrar y
+                continuemos con el mismo entusiasmo.
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </>
   );
 }
