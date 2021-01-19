4d016ad7289f36601826863bb317dcfd3826f14c
joma <johanmfc11@gmail.com>
2021-01-19 17:04:39 -0400

    there is no card coins

--- pages/cierre.js
+++ pages/cierre.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useContext } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CardMoney from '../components/Cierre/CardMoney';
-import { Button, Grid, Typography } from '@material-ui/core';
-import CardCoins from '../components/Cierre/CardCoins';
-import CierreContext from '../context/cierre/CierreContext';
+import React, { useEffect, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CardMoney from "../components/Cierre/CardMoney";
+import { Button, Grid, Typography } from "@material-ui/core";
+import CardCoins from "../components/Cierre/CardCoins";
+import CierreContext from "../context/cierre/CierreContext";
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(3),
@@ -36,30 +36,30 @@ const Cierre = () => {
             <CardCoins />
           </Grid>
           <Grid item xs={12}>
-            <Grid container justify='flex-start' spacing={2}>
+            <Grid container justify="flex-start" spacing={2}>
               <Grid item xs={3}>
-                <Typography variant='h2' component='div'>
+                <Typography variant="h2" component="div">
                   Total Efectivo = Bs {totalEfectivo}
                 </Typography>
               </Grid>
               <Grid item xs={3}>
-                <Typography variant='h2' component='div'>
-                  Total Efectivo Ventas = Bs{' '}
-                  {localStorage.getItem('totalEfectivo')
-                    ? localStorage.getItem('totalEfectivo')
+                <Typography variant="h2" component="div">
+                  Total Efectivo Ventas = Bs{" "}
+                  {localStorage.getItem("totalEfectivo")
+                    ? localStorage.getItem("totalEfectivo")
                     : 0}
                 </Typography>
               </Grid>
               <Grid item xs={3}>
-                <Typography variant='h2' component='div'>
-                  Total Tarjeta = Bs{' '}
-                  {localStorage.getItem('totalTarjeta')
-                    ? localStorage.getItem('totalTarjeta')
+                <Typography variant="h2" component="div">
+                  Total Tarjeta = Bs{" "}
+                  {localStorage.getItem("totalTarjeta")
+                    ? localStorage.getItem("totalTarjeta")
                     : 0}
                 </Typography>
               </Grid>
               <Grid item xs={3}>
-                <Button variant='contained' color='primary'>
+                <Button variant="contained" color="primary">
                   Guardar
                 </Button>
               </Grid>
